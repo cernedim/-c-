3bf4bf160d68e0f70db28ff663e4de4ba06d0048
Dima <fsgz@mail.ru>
2023-11-19 17:48:06 +0200

    max

--- Example_001_HrlloConsole/6giri/Program.cs
+++ Example_001_HrlloConsole/6giri/Program.cs
@@ -1,13 +1,27 @@
-﻿int a = 1;
-int b = 2;
-int c = 4;
-int d = 9;
-int e = 6;
-int max = a;
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-if (d > max) max = d;
-if (e > max) max = e;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 1;
+int b1 = 2;
+int c1 = 4;
+int a2 = 9;
+int b2 = 6;
+int c2 = 3;
+int a3 = 59;
+int b3 = 7;
+int c3 = 8;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
 Console.Write("max = ");
 Console.WriteLine(max);
