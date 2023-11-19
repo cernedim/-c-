047f1bbc4653a63dabcb25b06659c10d991cb515
Dima <fsgz@mail.ru>
2023-11-19 18:51:07 +0200

    написали метод заполнения массива рандомными элементами

---
+++ Example_001_HrlloConsole/massivv/Program.cs
@@ -0,0 +1,27 @@
+﻿void Fillmass(int[] collection)
+{
+     int Length = collection.Length;
+     int index = 0;
+     while (index < Length)
+     {
+        collection[index] = new Random().Next(1, 888);
+        //index = index + 1;
+        index++;
+     }
+}
+
+void Printmass(int[] col)
+{
+     int count = col.Length;
+     int position = 0;
+     while (position < count)
+     {
+        Console.WriteLine(col[position]);
+        position++;
+     }
+}
+
+int[] mass = new int[15];
+
+Fillmass(mass);
+Printmass(mass);
