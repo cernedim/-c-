7bbd0a91c47c2c4493b103360eaea468b334d2ba
Dima <fsgz@mail.ru>
2023-11-19 05:32:25 +0200

    привет маша

---
+++ Example_001_HrlloConsole/MAsha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("УРА! ЭТО ЖЕ МАША!!!");
+}
+else
+{
+    Console.WriteLine("Добро пожаловать");
+    Console.WriteLine(username);
+}
