d60d2cd962e46018024a6f981ef6e48c228e29e1
Dima <fsgz@mail.ru>
2023-11-19 05:08:10 +0200

    написали програму деления вещественных чисел

--- Example_001_HrlloConsole/Examle_004/Program.cs
+++ Example_001_HrlloConsole/Examle_004/Program.cs
@@ -1,3 +1,5 @@
-﻿double numbA = 17;
-double numbB = 6;
+﻿double numbA = new Random().Next(1, 100);
+Console.WriteLine(numbA);
+double numbB = new Random().Next(1, 100);
+Console.WriteLine(numbB);
 Console.WriteLine(numbA / numbB);
